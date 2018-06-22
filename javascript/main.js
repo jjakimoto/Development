2c63b5c6b692560528b649c2cdb3041299b385a7
jjakimoto <f.j.akimoto@gmail.com>
2018-06-21 23:08:26 -0400

    Update

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('btn1').on('mouseover', function() {
+            $('#panel1').toggle();
+        });
+    });
+</script>
