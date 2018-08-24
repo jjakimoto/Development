0a65073428661510cb240d35d230742ae2c7cca3
jjakimoto <f.j.akimoto@gmail.com>
2018-08-23 23:26:45 -0400

    Implement Integrated Gradient

--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('btn1').on('mouseover', function() {
-            $('#panel1').toggle();
-        });
-    });
-</script>
